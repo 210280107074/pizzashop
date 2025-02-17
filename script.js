4ec874a6d565ebe60e4bf104290ea8f648fdad80
Dhruvil_chotaliya <121554149+210280107074@users.noreply.github.com>
2025-02-17 15:36:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,9 +56,10 @@ let score = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("next").addEventListener("click", change_q);
-  });
+});
+
 const radioButtons = document.getElementsByClassName('radio-btn');
-// console.log(radioButtons);
+
 function change_q() {
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
@@ -66,22 +67,36 @@ function change_q() {
             if(radioButton.value ==  questions[index].answer){
                 score++;
             }
-            break;
+            
         }
     }
    index++;
     if (index < questions.length) {
         document.querySelector(".question").innerHTML = questions[index].question;
         document.querySelector(".opt-1").textContent = questions[index].options[0];
-        document.querySelector("#r1").textContent = questions[index].options[0];
+        document.querySelector("#r1").value = questions[index].options[0];
         document.querySelector(".opt-2").textContent = questions[index].options[1];
-        document.querySelector("#r2").textContent = questions[index].options[1];
+        document.querySelector("#r2").value = questions[index].options[1];
         document.querySelector(".opt-3").textContent = questions[index].options[2];
-        document.querySelector("#r3").textContent = questions[index].options[2];
+        document.querySelector("#r3").value = questions[index].options[2];
         document.querySelector(".opt-4").textContent = questions[index].options[3];
-        document.querySelector("#r4").textContent = questions[index].options[3];
+        document.querySelector("#r4").value = questions[index].options[3];
+        document.querySelector(".result").textContent = "Result : "  +score + "/" + index;
     } else {
-        document.querySelector(".result").textContent = "Result : "  +score + "/10";
         index = 0;
     }
+}
+
+function start(){
+    console.log("hiiii");
+    document.getElementById("card").style.display = "block";
+    document.querySelector(".question").innerHTML = questions[0].question;
+        document.querySelector(".opt-1").textContent = questions[0].options[0];
+        document.querySelector("#r1").value = questions[0].options[0];
+        document.querySelector(".opt-2").textContent = questions[0].options[1];
+        document.querySelector("#r2").value = questions[0].options[1];
+        document.querySelector(".opt-3").textContent = questions[0].options[2];
+        document.querySelector("#r3").value = questions[0].options[2];
+        document.querySelector(".opt-4").textContent = questions[0].options[3];
+        document.querySelector("#r4").value = questions[0].options[3];
 }
